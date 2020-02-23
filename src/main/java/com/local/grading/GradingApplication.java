d0ee9fd8e680e2ed4533d518b4b50f852876f849
caishen <caishen@greatonce.com>
2020-02-23 15:16:39 +0800

    第一次提交

---
+++ src/main/java/com/local/grading/GradingApplication.java
@@ -0,0 +1,13 @@
+package com.local.grading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/local/grading/GradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.grading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
