930fbc9e3de56848597bd66b965e3a50d4b06188
caishen <caishen@greatonce.com>
2020-03-07 22:05:36 +0800

    重构项目

--- src/test/java/com/local/grading/mapper/RegionMapperTest.java
+++ src/test/java/com/local/grading/mapper/RegionMapperTest.java
@@ -1,7 +1,5 @@
 package com.local.grading.mapper;
 
-import com.local.grading.domain.Region;
-import com.local.grading.service.RegionService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
